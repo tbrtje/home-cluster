c1482dd89de66437807028eb74c542885761d32a
Thies Johann Brötje <kontakt@thiesbroetje.de>
2022-03-29 15:19:41 +0200

    made a new branch for the new cluster
    Signed-off-by: Thies Johann Brötje <kontakt@thiesbroetje.de>

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -5,11 +5,11 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "3.11.0"
     }
-    http = {
+    http       = {
       source  = "hashicorp/http"
       version = "2.1.0"
     }
-    sops = {
+    sops       = {
       source  = "carlpett/sops"
       version = "0.6.3"
     }
@@ -25,17 +25,11 @@ provider "cloudflare" {
   api_key = data.sops_file.cloudflare_secrets.data["cloudflare_apikey"]
 }
 
-data "cloudflare_zones" "domain" {
-  filter {
-    name = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-  }
-}
-
 resource "cloudflare_zone_settings_override" "cloudflare_settings" {
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  zone_id = lookup(data.cloudflare_zones.main_domain.zones[0], "id")
   settings {
     # /ssl-tls
-    ssl = "strict"
+    ssl                      = "strict"
     # /ssl-tls/edge-certificates
     always_use_https         = "on"
     min_tls_version          = "1.0"
@@ -44,33 +38,33 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
     automatic_https_rewrites = "on"
     universal_ssl            = "on"
     # /firewall/settings
-    browser_check  = "on"
-    challenge_ttl  = 1800
-    privacy_pass   = "on"
-    security_level = "medium"
+    browser_check            = "on"
+    challenge_ttl            = 1800
+    privacy_pass             = "on"
+    security_level           = "medium"
     # /speed/optimization
-    brotli = "on"
+    brotli                   = "on"
     minify {
       css  = "on"
       js   = "on"
       html = "on"
     }
-    rocket_loader = "on"
+    rocket_loader            = "on"
     # /caching/configuration
-    always_online    = "off"
-    development_mode = "off"
+    always_online            = "off"
+    development_mode         = "off"
     # /network
-    http3               = "on"
-    zero_rtt            = "on"
-    ipv6                = "on"
-    websockets          = "on"
-    opportunistic_onion = "on"
-    pseudo_ipv4         = "off"
-    ip_geolocation      = "on"
+    http3                    = "on"
+    zero_rtt                 = "on"
+    ipv6                     = "on"
+    websockets               = "on"
+    opportunistic_onion      = "on"
+    pseudo_ipv4              = "off"
+    ip_geolocation           = "on"
     # /content-protection
-    email_obfuscation   = "on"
-    server_side_exclude = "on"
-    hotlink_protection  = "off"
+    email_obfuscation        = "on"
+    server_side_exclude      = "on"
+    hotlink_protection       = "off"
     # /workers
     security_header {
       enabled = false
@@ -80,31 +74,4 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
 
 data "http" "ipv4" {
   url = "http://ipv4.icanhazip.com"
-}
-
-resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = chomp(data.http.ipv4.body)
-  proxied = true
-  type    = "A"
-  ttl     = 1
-}
-
-resource "cloudflare_record" "root" {
-  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
-
-resource "cloudflare_record" "hajimari" {
-  name    = "hajimari"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
 }
